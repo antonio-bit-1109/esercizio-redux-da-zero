14f0e5b4e8d1ee925615196adeeef7ebe70a957b
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-01-16 13:42:28 +0100

    gg

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainComponent from "./components/MainComponent";
 
-function App() {
-    return <div className="App"></div>;
-}
+const App = () => {
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<MainComponent />} />
+                </Routes>
+            </BrowserRouter>
+            ;
+        </>
+    );
+};
 
 export default App;
--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setFirstState } from "../redux/reducers/StateReducer";
 
 const MainComponent = () => {
-    return <div> ciao ciao bello!! </div>;
+    /* dispatch per fare il setState dello stato */
+    const dispatch = useDispatch();
+    const primoStato = useSelector((state) => state.State.firstState);
+    console.log(primoStato);
+
+    return (
+        <div>
+            <button onClick={() => dispatch(setFirstState("ma stiamo scherziamo! "))}> cliccami</button>
+        </div>
+    );
 };
 
 export default MainComponent;
